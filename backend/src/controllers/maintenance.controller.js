1dd5f3f35165f7a5aeb7f7c97f059b838bc9b018
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2024-02-21 08:44:00 -0300

    add error handler with try-catch

--- backend/src/controllers/maintenance.controller.js
+++ backend/src/controllers/maintenance.controller.js
@@ -1,47 +1,58 @@
 const Device = require("../models/device.model")
 const Maintenance = require("../models/maintenances.model")
 
-const getAllMaintenances = async (req, res) => {
-	const maintenances = await Maintenance.find({})
-
-	return res.json(maintenances)
+const getAllMaintenances = async (req, res, next) => {
+	try {
+		const maintenances = await Maintenance.find({})
+		return res.json(maintenances)
+	} catch (err) {
+		next(err)
+	}
 }
 
-const addMaintenance = async (req, res) => {
+const addMaintenance = async (req, res, next) => {
 	const { date, detail, observation } = req.body
 	const { deviceId } = req.params
 
-	const newMaintenance = await Maintenance.create({
-		device_id: deviceId,
-		date,
-		detail,
-		observation,
-	})
+	try {
+		const newMaintenance = await Maintenance.create({
+			device_id: deviceId,
+			date,
+			detail,
+			observation,
+		})
 
-	// update maintenances array in Device model
-	const device = await Device.findById(deviceId)
-	device.maintenances.push(newMaintenance)
-	await device.save()
+		// update maintenances array in Device model
+		const device = await Device.findById(deviceId)
+		device.maintenances.push(newMaintenance)
+		await device.save()
 
-	return res.status(201).json(newMaintenance)
+		return res.status(201).json(newMaintenance)
+	} catch (err) {
+		next(err)
+	}
 }
 
-const deleteMaintenance = async (req, res) => {
+const deleteMaintenance = async (req, res, next) => {
 	const { id } = req.params
 
-	const maintenanceToDelete = await Maintenance.findById(id)
-	if (!maintenanceToDelete)
-		return res.status(404).json({ ok: false, error: `No se encontró registro con el id ${id}` })
+	try {
+		const maintenanceToDelete = await Maintenance.findById(id)
+		if (!maintenanceToDelete)
+			return res.status(404).json({ ok: false, error: `No se encontró registro con el id ${id}` })
 
-	const device = await Device.findById(maintenanceToDelete.device_id)
-	device.maintenances = device.maintenances.filter(
-		(maintenanceId) => maintenanceId.toString() !== id
-	)
-	await device.save()
+		const device = await Device.findById(maintenanceToDelete.device_id)
+		device.maintenances = device.maintenances.filter(
+			(maintenanceId) => maintenanceId.toString() !== id
+		)
+		await device.save()
 
-	await Maintenance.findByIdAndDelete(id)
+		await Maintenance.findByIdAndDelete(id)
 
-	return res.status(204).json({ ok: true })
+		return res.status(204).json({ ok: true })
+	} catch (err) {
+		next(err)
+	}
 }
 
 module.exports = { getAllMaintenances, addMaintenance, deleteMaintenance }
